8403496b6179e9d20e3e1da5b34696dfd9d660de
Bartosz Madej <johnnythemadman@gmail.com>
2019-11-18 22:10:20 +0100

    admin setup

--- RPG/catalog/admin.py
+++ RPG/catalog/admin.py
@@ -2,5 +2,14 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Race)
+admin.site.register(Statistics)
+admin.site.register(Condition)
+admin.site.register(Skill)
+admin.site.register(Entity)
+admin.site.register(Npc)
+admin.site.register(Character)
+admin.site.register(User)
